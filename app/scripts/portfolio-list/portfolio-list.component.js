ac9e3322b4253b6f673ff145255daf0c5ba3bd1b
Sergiu Ciobanasu <szergen@gmail.com>
2016-08-16 16:42:52 +0300

    Added the scroll to portfolio functionality to the "let's see it" button by creating a scrollTo function in the controller with the $location and $anchorScroll services.

--- app/scripts/portfolio-list/portfolio-list.component.js
+++ app/scripts/portfolio-list/portfolio-list.component.js
@@ -2,24 +2,31 @@ angular.
  module('portfolioList').
   component('portfolioList', {
     templateUrl: 'views/main.html',
-    controller: ['$http', '$filter', 
-        function PortfolioListController($http, $filter){
+    controller: ['$http', '$location', '$anchorScroll', 
+        function PortfolioListController($http, $location, $anchorScroll){
             
             self = this;
             self.myVar = "tralala2213";
             $http.get('scripts/data.json').then(function(response) {
                 self.projects = response.data;
             });
+            
             self.changeClass = function(){
                 $('li.active').removeClass('active');
-                 
             }
+            
             self.changeClass2 = function(a){
-               
                 $('.nav li').removeClass('active');
                 $('[data-cat='+a+']').addClass('active'); 
                 console.log(a);
             }
-            
+            self.scrollTo = function(id) {
+                var old = $location.hash();
+                $location.hash(id);
+                $anchorScroll();
+                //reset to old to keep any additional routing logic from kicking in
+                $location.hash(old);
+                console.log("Merge, baaa!!!");
+            }
     }]
 });
